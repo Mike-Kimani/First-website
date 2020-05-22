e3397f6c2e2442348cb5db65522e582cb268bbf9
Mike-Kimani <mikalkimmani@gmail.com>
2020-05-22 16:18:01 +0300

    Getting user entered values

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function alertMessage(message){
-    alert(message)
+let personDetails = {
+    fullName: {
+        firstName : 'John',
+        lastName : 'Kimani'
+    },
+    alertHer: function(message){
+        alert(message);
+    }
 }
-alertMessage('Welcome')
+personDetails.alertHer('Hello there')
+
+let submitButton = document.querySelector('#submit-button');
+
+function clickListener(event){
+event.preventDefault();
+
+console.log('Email: ', document.querySelector('#email').value);
+console.log('message:', document.querySelector('#message').value);
+}
+submitButton.addEventListener('click', clickListener);
