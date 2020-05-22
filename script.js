17b24575fe3f63849d82b497570853b75b646c5f
Mike-Kimani <mikalkimmani@gmail.com>
2020-05-22 18:33:19 +0300

    Added email checker

--- script.js
+++ script.js
@@ -11,10 +11,27 @@ personDetails.alertHer('Hello there')
 
 let submitButton = document.querySelector('#submit-button');
 
+
+function validateEmail(email){
+    if (email.includes('@')){
+return true;
+    }
+    else
+    return false;
+}
 function clickListener(event){
 event.preventDefault();
 
-console.log('Email: ', document.querySelector('#email').value);
+let emailText = document.querySelector('#email').value;
+console.log('Email: ', emailText);
 console.log('message:', document.querySelector('#message').value);
+if(validateEmail(emailText) !== true){
+    console.log('Your email address must include an @')
+    return false;
+}
+console.log('Thank you for the reply')
 }
 submitButton.addEventListener('click', clickListener);
+
+
+
